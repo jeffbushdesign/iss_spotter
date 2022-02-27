509958bb5e7fb063e2fdfbeff3510ce4702c83cb
Jeff Bush <jeffbushdesign@gmail.com>
2022-02-27 03:35:45 +0000

    Simplified index

--- index.js
+++ index.js
@@ -24,15 +24,28 @@ fetchCoordsByIP('192.0.231.222', (error, coordinates) => {
   console.log('It worked! Here are your coordinates: ', coordinates);
 });
 
-const enterCoords = { latitude: '45.4499', longitude: '-73.8104' };
 
-fetchISSFlyOverTimes(enterCoords, (error, flyOverTimes) => {
+// Geo Coordinates to use with fetchISSFlyOverTimes
+const geoCoords = { latitude: '45.4499', longitude: '-73.8104' };
+
+// Define the callbackFunction
+const callbackFunction = function (error, flyOverTimes) {
   if (error) {
     console.log("It didn't work!", error);
     return;
   }
-
   console.log('It worked! Here are your flyover times: ', flyOverTimes);
-});
+};
+
+// Call the function
+fetchISSFlyOverTimes(geoCoords, callbackFunction);
 
 
+// (error, flyOverTimes) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log('It worked! Here are your flyover times: ', flyOverTimes);
+// }
